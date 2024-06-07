c967436260b022a7e9212b313e16e44bb4bd1ebe
Teages <Teages@teages.xyz>
2024-06-07 20:35:30 +0800

    fix(cli): silent not work

--- src/cli/logger.ts
+++ src/cli/logger.ts
@@ -4,6 +4,6 @@ import type { Config } from './config'
 export const logger = consola.withTag('@teages/gqf/cli')
 export function useLogger(config: Pick<Config, 'silent'>) {
   return config.silent
-    ? logger.create({ level: 999 })
+    ? logger.create({ level: 0 })
     : logger
 }
