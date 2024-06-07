679170e651a307142093001179a4b96b77f5c12d
Teages <Teages@teages.xyz>
2024-06-07 22:00:30 +0800

    chore(typed): export typed node

--- src/typed/index.ts
+++ src/typed/index.ts
@@ -2,4 +2,4 @@ export type { RequireQueryPart } from './parse/part'
 export type { GraphQueryFunction } from './query-func'
 export type { GraphQueryPartial } from './query-part'
 
-export type { ResultOf, VariablesOf } from './document-node'
+export type { ResultOf, VariablesOf, TypedQueryDocumentNode } from './document-node'
