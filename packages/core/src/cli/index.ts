eb5a7a15348166242dd2cd3968e5e70dc0123120
Teages <Teages@teages.xyz>
2024-08-15 01:48:37 +0800

    chore!: limit the effect scope of node api

--- packages/core/src/cli/index.ts
+++ packages/core/src/cli/index.ts
@@ -1,20 +1,2 @@
-import type { DefineSchema, Schemas, UserSchemaTypes } from '../schema'
-import type { Package } from '../utils/package'
-import { $enum, gqfn, gqp } from '../core'
-
 export { sync, type Output } from './sync'
 export type { Config, ClientConfig, SchemaConfig } from './config'
-
-export type LoadFromUrl<T extends string> =
-  T extends keyof Schemas
-    ? Schemas[T] extends UserSchemaTypes | DefineSchema<any>
-      ? Schemas[T]
-      : undefined
-    : undefined
-
-export function useSchema<T extends string>(
-  _url?: T,
-): Package<LoadFromUrl<T>> {
-  return { gqfn, gqp, $enum } as any
-}
-export type { Package as GQFnPackage }
--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,8 +1,21 @@
 import { $enum, gqfn, gqp } from './core'
-import type { CreateGQFn } from './utils/package'
+import type { CreateGQFn, Package } from './utils/package'
+import type { DefineSchema, Schemas, UserSchemaTypes } from './schema'
 
 export { $enum } from './core'
 export type { RequireQueryPart, ResultOf, VariablesOf } from './typed'
 
 export const createGQFn: CreateGQFn = () => ({ gqfn, gqp, $enum }) as any
-export { useSchema } from './cli'
+
+export function useSchema<T extends string>(
+  _url?: T,
+): Package<LoadFromUrl<T>> {
+  return { gqfn, gqp, $enum } as any
+}
+
+export type LoadFromUrl<T extends string> =
+  T extends keyof Schemas
+    ? Schemas[T] extends UserSchemaTypes | DefineSchema<any>
+      ? Schemas[T]
+      : undefined
+    : undefined
