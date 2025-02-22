b1e7318a7b37591bc7b9d7d2f9a2adc1d75a5c2e
Teages <Teages@teages.xyz>
2025-02-22 23:47:48 +0800

    chore!: drop cjs support

--- packages/core/build.config.ts
+++ packages/core/build.config.ts
@@ -2,9 +2,6 @@ import { defineBuildConfig } from 'unbuild'
 
 export default defineBuildConfig({
   declaration: true,
-  rollup: {
-    emitCJS: true,
-  },
   entries: [
     'src/index.ts',
     'src/runtime/index.ts',
