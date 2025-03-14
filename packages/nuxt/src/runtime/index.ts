acf2dbff8f0fb145f1f776162f3467c4052a1a34
Teages <Teages@teages.xyz>
2025-03-14 19:12:50 +0800

    chore(nuxt): mark gqfn/core as peer dependency

--- packages/nuxt/src/runtime/index.ts
+++ packages/nuxt/src/runtime/index.ts
@@ -1,3 +1,1 @@
-export type { LoadGQFnFromUrl, LoadSchemaFromUrl } from '@gqfn/core'
-export type { Schemas } from '@gqfn/core/schema'
 export type { RequireOperationPartialData, ResultOf, TypedDocumentNode, VariablesOf } from '@gqfn/core/types'
